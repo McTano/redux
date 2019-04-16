3d03607ae1f18b5d551e39ac9044b54e386085a0
Tristan MacKinlay <tsmackinlay@gmail.com>
2019-04-15 17:14:07 -0700

    ran prettier everywhere

--- examples/shopping-cart/src/components/Cart.js
+++ examples/shopping-cart/src/components/Cart.js
@@ -2,17 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Product from './Product'
 
-const Cart  = ({ products, total, onCheckoutClicked }) => {
+const Cart = ({ products, total, onCheckoutClicked }) => {
   const hasProducts = products.length > 0
   const nodes = hasProducts ? (
-    products.map(product =>
+    products.map(product => (
       <Product
         title={product.title}
         price={product.price}
         quantity={product.quantity}
         key={product.id}
       />
-    )
+    ))
   ) : (
     <em>Please add some products to cart.</em>
   )
@@ -21,9 +21,14 @@ const Cart  = ({ products, total, onCheckoutClicked }) => {
     <div>
       <h3>Your Cart</h3>
       <div>{nodes}</div>
-      <p>Total: &#36;{total}</p>
-      <button onClick={onCheckoutClicked}
-        disabled={hasProducts ? '' : 'disabled'}>
+      <p>
+        Total: &#36;
+        {total}
+      </p>
+      <button
+        onClick={onCheckoutClicked}
+        disabled={hasProducts ? '' : 'disabled'}
+      >
         Checkout
       </button>
     </div>
--- examples/shopping-cart/src/components/Product.js
+++ examples/shopping-cart/src/components/Product.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 
 const Product = ({ price, quantity, title }) => (
   <div>
-    {title} - &#36;{price}{quantity ? ` x ${quantity}` : null}
+    {title} - &#36;
+    {price}
+    {quantity ? ` x ${quantity}` : null}
   </div>
 )
 
